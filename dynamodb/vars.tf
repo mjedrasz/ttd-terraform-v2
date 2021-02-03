b625528e02e8e71ab004aa6713adf17e5bfb7146
Marek Jedraszewski <marek.jedraszewski@ing.com>
2021-02-03 20:12:51 +0100

    dynamodb default capacities

--- dynamodb/vars.tf
+++ dynamodb/vars.tf
@@ -25,14 +25,16 @@ variable "table_name" {
 
 variable "read_capacity" {
   description = "Read capacity"
+  default = 1
 }
 
 variable "write_capacity" {
   description = "Write capacity"
+  default = 1
 }
 
 variable "billing_mode" {
-  description = "Billing mode"
+  description = "Billing mode"  
 }
 
 
