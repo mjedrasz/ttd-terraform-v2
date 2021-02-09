151d4802af2a05164f82fa60772ceb0133766911
Marek Jedraszewski <marek.jedraszewski@ing.com>
2021-02-09 20:17:06 +0100

    block all public access

--- s3web/main.tf
+++ s3web/main.tf
@@ -26,6 +26,13 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
   policy =<<POLICY
